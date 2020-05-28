8c64769b84563108656a66b5bc565544dd158c5e
Cynthia Chen <cynthia@roamandwander.com>
2020-05-28 15:16:00 +0800

    fixed iOS13 scene delegate issue

--- rolo/AppDelegate.swift
+++ rolo/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISceneDelegate {
 
     var window: UIWindow?
 
@@ -36,7 +36,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    //MARK: UISceneDelegate
+    
+    @available(iOS 13.0, *)
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = UINavigationController(rootViewController: ChatListViewController())
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
+    }
 }
 
